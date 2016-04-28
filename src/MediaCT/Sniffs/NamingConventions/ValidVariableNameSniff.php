e78d0f13e5e545d3a80ba100939ab7e41d4c06c7
Klaas Sangers <klaas@sangers.nu>
2016-04-28 15:40:48 +0200

    added check on variable name

---
+++ src/MediaCT/Sniffs/NamingConventions/ValidVariableNameSniff.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright MediaCT. All rights reserved.
+ * https://www.mediact.nl
+ */
+
+namespace MediaCT\Sniffs\NamingConventions;
+
+use PHP_CodeSniffer_File;
+use PHP_CodeSniffer_Sniff;
+
+class ValidVariableNameSniff implements PHP_CodeSniffer_Sniff
+{
+
+    /**
+     * Listen to variable name tokens.
+     *
+     * @return int[]
+     */
+    public function register()
+    {
+        return [T_VARIABLE];
+    }
+
+    /**
+     * Check variable names to make sure no underscores are used.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile
+     * @param int                  $stackPtr
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens  = $phpcsFile->getTokens();
+        $varName = ltrim($tokens[$stackPtr]['content'], '$');
+
+        if (preg_match('/^_/', $varName)) {
+            $phpcsFile->addFixableWarning(
+                'Variable names may not start with an underscore',
+                $stackPtr,
+                'IllegalVariableNameUnderscore'
+            );
+        }
+    }
+}
